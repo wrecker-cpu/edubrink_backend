b6db83d2d0a1d4553eb8a7a7c64762806b900069
karan <jangdekaran54@gmail.com>
2024-12-28 10:15:25 +0530

    updateUniveristy

--- routes/UniversityRoutes.js
+++ routes/UniversityRoutes.js
@@ -2,7 +2,7 @@ const universityController = require("../controllers/UniversityController");
 const router = require("express").Router();
 
 // Route for getting, updating, and deleting a user by ID
-router.get("/", universityController.createUniversity);
+router.post("/", universityController.createUniversity);
 router.put("/:id", universityController.updateUniversity);
 router.get("/:id", universityController.getUniversityById);
 router.get("/", universityController.getAllUniversities);
